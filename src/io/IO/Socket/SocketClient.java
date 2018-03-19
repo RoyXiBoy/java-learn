1eabc321e1350524dc7440261ab7104cfb79eb03
xlt <aa543187001@163.com>
2018-03-19 18:00:27 +0800

    NIO

---
+++ src/io/IO/Socket/SocketClient.java
@@ -0,0 +1,29 @@
+package io.IO.Socket;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+
+/**
+ * Created by xlt on 20180319.
+ */
+public class SocketClient {
+
+    public static void main(String[] args) {
+
+        String host = "127.0.0.1";
+
+        int port = 60001;
+
+        try(Socket socket = new Socket(host, port);
+            OutputStream outputStream = socket.getOutputStream()){
+
+            String message = "hello world";
+
+            socket.getOutputStream().write(message.getBytes());
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
--- src/io/IO/Socket/SocketServer.java
+++ src/io/IO/Socket/SocketServer.java
@@ -1,58 +1,39 @@
 package io.IO.Socket;
 
-import com.sun.security.ntlm.Server;
 
-import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.InetAddress;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.UnknownHostException;
 
 /**
  * Created by xlt on 20180316.
  */
 public class SocketServer {
-    /*public static void main(String[] args) throws Exception {
-        // 监听指定的端口
-        int port = 55533;
-        ServerSocket server = new ServerSocket(port);
-
-        // server将一直等待连接的到来
-        System.out.println("server将一直等待连接的到来");
-        Socket socket = server.accept();
-        // 建立好连接后，从socket中获取输入流，并建立缓冲区进行读取
-        InputStream inputStream = socket.getInputStream();
-        byte[] bytes = new byte[1024];
-        int len;
-        StringBuilder sb = new StringBuilder();
-        while ((len = inputStream.read(bytes)) != -1) {
-            //注意指定编码格式，发送方和接收方一定要统一，建议使用UTF-8
-            sb.append(new String(bytes, 0, len,"UTF-8"));
-        }
-        System.out.println("get message from client: " + sb);
-        inputStream.close();
-        socket.close();
-        server.close();
-    }*/
 
     public static void main(String[] args) {
-        int port = 60001;
-        try {
-            InetAddress inetAddress = InetAddress.getByAddress(new byte[]{127,0,0,1});
-        } catch (UnknownHostException e) {
+
+       int port = 60001;
+
+        try (ServerSocket server = new ServerSocket(port);
+             Socket socket = server.accept();
+             //建立好连接后，从socket中获取输入流，并建立缓冲区进行读取
+             InputStream inputStream = socket.getInputStream()){
+
+            System.out.println("server将一直等待连接的到来");
+            byte[] bytes = new byte[1024];
+            int len;
+
+            StringBuilder sb = new StringBuilder();
+            while ((len = inputStream.read(bytes)) != -1) {
+                //注意指定编码格式，发送方和接收方一定要统一，建议使用UTF-8
+                sb.append(new String(bytes, 0, len,"UTF-8"));
+            }
+            System.out.println("get message from client: " + sb);
+        } catch (IOException e) {
             e.printStackTrace();
         }
-
-        try(ServerSocket serverSocket = new ServerSocket(port);
-            Socket socket = serverSocket.accept();
-            BufferedInputStream bufferedInputStream = new BufferedInputStream(socket.getInputStream())){
-
-        }catch (IOException e){
-            e.printStackTrace();
-        }
-
-
     }
 }
